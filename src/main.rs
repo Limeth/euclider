b461dbcc0067d680deaeec1d0825bdf5d3d504bf
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-05-15 11:47:51 +0200

    make it compile!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate glium;
 mod universe;
 
-use universe::GDLUniverse;
+use glium::DisplayBuild;
+use universe::{Universe, Universe3D};
 
 fn main() {
     println!("Hello, world!");
+    let game = Game::builder()
+        .with_universe(Box::new(Universe3D::new()))
+        .build();
+
+    game.start();
 }
 
 pub struct Game {
@@ -17,8 +24,12 @@ struct GameBuilder {
 impl Game {
     fn new(builder: GameBuilder) -> Game {
         Game {
-            universe: builder.universe,
+            universe: builder.universe.unwrap(),
         }
+    }
+
+    fn start(self) {
+        let display = glium::glutin::WindowBuilder::new();
     }
 
     fn builder() -> GameBuilder {
@@ -29,7 +40,12 @@ impl Game {
 }
 
 impl GameBuilder {
-    fn with_universe(&mut self, universe: Universe) {
-        self.universe = Box::new(universe);
+    fn with_universe(mut self, universe: Box<Universe>) -> GameBuilder {
+        self.universe = Some(universe);
+        self
+    }
+
+    fn build(self) -> Game {
+        Game::new(self)
     }
 }
--- src/universe.rs
+++ src/universe.rs
@@ -1,16 +1,26 @@
 pub trait Universe {
-    fn render(&self);
-    fn update(&mut self, delta: f32);
+    fn render(&self, time: f32);
+    fn update(&mut self, delta_time: f32);
 }
 
-pub struct GDLUniverse {
+pub struct Universe3D {
 
 }
 
-impl GDLUniverse {
-    fn new() -> GDLUniverse {
-        GDLUniverse {
+impl Universe3D {
+    pub fn new() -> Universe3D {
+        Universe3D {
 
         }
+    }
+}
+
+impl Universe for Universe3D {
+    fn render(&self, time: f32) {
+
+    }
+
+    fn update(&mut self, delta_time: f32) {
+
     }
 }

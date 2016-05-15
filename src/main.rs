d9d75a064449e8e2a7340817f31fb5c525a738c6
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-05-15 19:55:42 +0200

    relocate RemoveIf

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 extern crate glium;
 mod universe;
+pub mod util;
 
 use std::collections::HashSet;
 use std::time::Instant;
@@ -16,6 +17,7 @@ use glium::glutin::Event;
 use glium::Surface;
 use universe::Universe;
 use universe::Universe3D;
+use util::RemoveIf;
 
 pub struct Simulation<U: Universe> {
     universe: Box<U>,
@@ -151,30 +153,6 @@ impl SimulationContext {
             }
             print_type_of(&event);
         }
-    }
-}
-
-trait RemoveIf<T, C> {
-    fn remove_if<F>(&mut self, f: F) -> C where F: Fn(&T) -> bool;
-}
-
-use std::hash::Hash;
-
-impl<T> RemoveIf<T, HashSet<T>> for HashSet<T> where T: Eq + Copy + Hash {
-    fn remove_if<F>(&mut self, f: F) -> HashSet<T> where F: Fn(&T) -> bool {
-        let mut removed: HashSet<T> = HashSet::new();
-
-        for value in self.iter() {
-            if f(value) {
-                removed.insert(value.clone());
-            }
-        }
-
-        for removed_value in &removed {
-            self.remove(&removed_value);
-        }
-
-        removed
     }
 }
 
---
+++ src/util.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub trait RemoveIf<T, C> {
+    fn remove_if<F>(&mut self, f: F) -> C where F: Fn(&T) -> bool;
+}
+
+impl<T> RemoveIf<T, HashSet<T>> for HashSet<T> where T: Eq + Copy + Hash {
+    fn remove_if<F>(&mut self, f: F) -> HashSet<T> where F: Fn(&T) -> bool {
+        let mut removed: HashSet<T> = HashSet::new();
+
+        for value in self.iter() {
+            if f(value) {
+                removed.insert(value.clone());
+            }
+        }
+
+        for removed_value in &removed {
+            self.remove(&removed_value);
+        }
+
+        removed
+    }
+}

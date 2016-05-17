98168ab961c21636e2e2ddc241116118ba82ee03
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-05-17 20:31:35 +0200

    install dependencies automatically

--- ci/install.sh
+++ ci/install.sh
@@ -59,8 +59,8 @@ main() {
     echo ARCHITECTURES:
     dpkg --print-foreign-architectures
     sudo apt-get install -y libc6:i386
-    sudo apt-get install -y ia32-libs-multiarch
     sudo apt-get install -y ia32-libs
+    sudo apt-get -fy install
 }
 
 main

e2de81e888dcc31e70fcb064d188e90ef5144b05
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-05-17 19:57:37 +0200

    skip confirmation when installing requirements

--- ci/install.sh
+++ ci/install.sh
@@ -55,11 +55,11 @@ main() {
     configure_cargo
 
     # TODO if you need to install extra stuff add it here
-    sudo dpkg --add-architecture i386
+    # sudo dpkg --add-architecture i386
     echo ARCHITECTURES:
     dpkg --print-foreign-architectures
-    sudo apt-get install libc6:i386
-    sudo apt-get install ia32-libs
+    sudo apt-get install -y libc6:i386
+    sudo apt-get install -y ia32-libs
 }
 
 main

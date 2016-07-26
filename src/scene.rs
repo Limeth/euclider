491d99fd64d065f270d63bcab5ee632927829156
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-07-26 23:18:11 +0200

    deserialize material transition handlers

--- src/scene.rs
+++ src/scene.rs
@@ -1,33 +1,42 @@
 use std::any::Any;
+use std::any::TypeId;
 use std::collections::HashMap;
 use util::CustomFloat;
 use universe::Environment;
+use universe::Universe;
 use universe::d3::Universe3D;
 use json;
 use json::JsonValue;
 use json::object::Object;
 use mopa;
+use universe::entity::material;
+use universe::entity::material::TransitionHandler;
+use universe::entity::material::Vacuum;
+use na::Point3;
+use na::Vector3;
 
-pub type ConstructorFunction<T> = Constructor<Box<[Box<Any>]>, T>;
-pub type ConstructorStruct<T> = Constructor<HashMap<String, Box<Any>>, T>;
-pub type Constructor<A, T> = Fn(A) -> T;
 pub type Deserializer<T> = Fn(&JsonValue, &Parser) -> Result<T, ParserError>;
 
 #[derive(Debug)]
 pub enum ParserError {
-    NoDeserializer(String),
-    TypeMismatch(String, Box<Any + 'static>),
-    SyntaxError(String, json::Error),
-    InvalidStructure,
-}
-
-pub struct Legend<T> {
-    constructor_struct: Option<Box<ConstructorStruct<T>>>,
-    constructor_functions: Vec<(String, Box<ConstructorFunction<T>>)>,
-}
-
-pub trait Prop: Sized {
-    fn get_legend() -> Legend<Self>;
+    NoDeserializer {
+        key: String,
+    },
+    TypeMismatch {
+        key: String,
+        value: Box<Any + 'static>,
+    },
+    SyntaxError {
+        key: String,
+        error: json::Error,
+    },
+    InvalidStructure {
+        description: String,
+        json: JsonValue,
+    },
+    MissingType {
+        type_str: String,
+    }
 }
 
 pub trait Deserializable: mopa::Any {
@@ -36,36 +45,154 @@ pub trait Deserializable: mopa::Any {
 }
 
 pub struct Parser {
-    pub deserializers: HashMap<&'static str, Box<Deserializer<Box<Any>>>>
+    pub deserializers: HashMap<&'static str, Box<Deserializer<Box<Any>>>>,
+    pub type_map: HashMap<&'static str, TypeId>,
 }
 
 impl Parser {
     pub fn empty() -> Self {
         Parser {
             deserializers: HashMap::new(),
+            type_map: HashMap::new(),
         }
     }
 
+    #[allow(unused_variables)]
     pub fn default<F: CustomFloat>() -> Self {
         let mut parser = Self::empty();
 
         {
+            let type_map = &mut parser.type_map;
+
+            // Materials
+
+            type_map.insert("Vacuum", TypeId::of::<Vacuum>());
+        }
+
+        {
             let deserializers = &mut parser.deserializers;
+
+            // Materials
+
+            deserializers.insert("Vacuum",
+                                 Box::new(|json: &JsonValue, parser: &Parser| {
+                                     Ok(Box::new(Vacuum::new()))
+                                 }));
+
+            // Material transition handlers
+
+            deserializers.insert("transition_3d_vacuum_vacuum",
+                                 Box::new(|json: &JsonValue, parser: &Parser| {
+                                     let value: Box<TransitionHandler<F, Point3<F>, Vector3<F>>> = 
+                                         Box::new(material::transition_vacuum_vacuum::<F, Point3<F>, Vector3<F>>);
+                                     Ok(Box::new(value))
+                                 }));
+
+            // Environments
 
             deserializers.insert("Environment",
                                  Box::new(|json: &JsonValue, parser: &Parser| {
                                      let object: &Object = if let JsonValue::Object(ref object) = *json {
                                          object
                                      } else {
-                                         return Err(ParserError::InvalidStructure);
+                                         return Err(ParserError::InvalidStructure {
+                                             description: "The JSON value must be an object.".to_owned(),
+                                             json: json.clone(),
+                                         });
                                      };
 
                                      let environment = object.get("environment").unwrap().as_str().unwrap();
 
-                                     println!("{}", environment);
+                                     let result: Box<Box<Environment<F>>> =
+                                         try!(parser.deserialize::<Box<Environment<F>>>(environment, json));
+
+                                     Ok(result)
+                                 }));
+
+            deserializers.insert("Universe3D",
+                                 Box::new(|json: &JsonValue, parser: &Parser| {
+                                     let object: &Object = if let JsonValue::Object(ref object) = *json {
+                                         object
+                                     } else {
+                                         return Err(ParserError::InvalidStructure {
+                                             description: "The JSON value must be an object.".to_owned(),
+                                             json: json.clone(),
+                                         });
+                                     };
+
+                                     let mut universe = Universe3D::<F>::new();
+
+                                     {
+                                         let json_transitions: &Vec<JsonValue> =
+                                             if let JsonValue::Array(ref json_transitions) = *object.get("transitions")
+                                                .expect("The array `transitions` is missing.") {
+                                             json_transitions
+                                         } else {
+                                             return Err(ParserError::InvalidStructure {
+                                                 description: "The field `transitions` is not an array.".to_owned(),
+                                                 json: json.clone(),
+                                             });
+                                         };
+
+                                         let universe_transitions = universe.transitions_mut();
+
+                                         for json_transition in json_transitions {
+                                             let json_transition = if let JsonValue::Object(ref json_transition) = *json_transition {
+                                                 json_transition
+                                             } else {
+                                                 return Err(ParserError::InvalidStructure {
+                                                     description: "The transition must be an object!".to_owned(),
+                                                     json: json_transition.clone(),
+                                                 });
+                                             };
+
+                                             let from_str = try!(try!(json_transition.get("from")
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "Missing a `from` field in transition definition.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }))
+                                                    .as_str()
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "The field `from` must be a string.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }));
+                                             let to_str = try!(try!(json_transition.get("to")
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "Missing a `to` field in transition definition.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }))
+                                                    .as_str()
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "The field `to` must be a string.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }));
+                                             let transition_field = try!(json_transition.get("transition")
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "Missing a `transition` field in transition definition.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }));
+                                             let transition_str = try!(transition_field                                                .as_str()
+                                                    .ok_or_else(|| ParserError::InvalidStructure {
+                                                        description: "The field `transition` must be a string.".to_owned(),
+                                                        json: JsonValue::Object(json_transition.clone()),
+                                                    }));
+                                             let from_id = try!(parser.type_map.get(from_str)
+                                                              .ok_or_else(|| ParserError::MissingType {
+                                                                  type_str: from_str.to_owned(),
+                                                              }));
+                                             let to_id = try!(parser.type_map.get(to_str)
+                                                              .ok_or_else(|| ParserError::MissingType {
+                                                                  type_str: to_str.to_owned(),
+                                                              }));
+                                             let transition = try!(parser
+                                                .deserialize::<Box<TransitionHandler<F, Point3<F>, Vector3<F>>>>(transition_str, transition_field));
+
+                                             universe_transitions.insert((*from_id, *to_id), *transition);
+                                         }
+                                     }
 
                                      let result: Box<Box<Environment<F>>> =
-                                         Box::new(Box::new(Universe3D::<F>::new()));
+                                         Box::new(Box::new(universe));
 
                                      Ok(result)
                                  }));
@@ -80,7 +207,9 @@ impl Parser {
         if option.is_some() {
             Ok(option.unwrap().as_ref())
         } else {
-            Err(ParserError::NoDeserializer(key.to_owned()))
+            Err(ParserError::NoDeserializer {
+                key: key.to_owned(),
+            })
         }
     }
 
@@ -90,7 +219,10 @@ impl Parser {
 
         match result {
             Ok(value) => Ok(value),
-            Err(err) => Err(ParserError::TypeMismatch(key.to_owned(), err))
+            Err(original_value) => Err(ParserError::TypeMismatch {
+                key: key.to_owned(),
+                value: original_value,
+            })
         }
     }
 
@@ -99,7 +231,10 @@ impl Parser {
 
         match value {
             Ok(value) => self.deserialize::<T>(key, &value),
-            Err(err) => Err(ParserError::SyntaxError(key.to_owned(), err))
+            Err(err) => Err(ParserError::SyntaxError {
+                key: key.to_owned(),
+                error: err,
+            })
         }
     }
 }

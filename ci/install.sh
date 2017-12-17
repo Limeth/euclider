73e96b8fdd89050802119cd8dc2248d00bc5749f
Jakub Hlusička <limeth@protonmail.com>
2017-12-17 16:57:12 +0100

    Attempt a CI update

--- ci/install.sh
+++ ci/install.sh
@@ -1,66 +1,47 @@
-# `install` phase: install stuff needed for the `script` phase
-
 set -ex
 
-. $(dirname $0)/utils.sh
+main() {
+    local target=
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        target=x86_64-unknown-linux-musl
+        sort=sort
+    else
+        target=x86_64-apple-darwin
+        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
+    fi
 
-install_c_toolchain() {
+    # Builds for iOS are done on OSX, but require the specific target to be
+    # installed.
     case $TARGET in
-        aarch64-unknown-linux-gnu)
-            sudo apt-get install -y --no-install-recommends \
-                 gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
+        aarch64-apple-ios)
+            rustup target install aarch64-apple-ios
             ;;
-        *)
-            # For other targets, this is handled by addons.apt.packages in .travis.yml
+        armv7-apple-ios)
+            rustup target install armv7-apple-ios
+            ;;
+        armv7s-apple-ios)
+            rustup target install armv7s-apple-ios
+            ;;
+        i386-apple-ios)
+            rustup target install i386-apple-ios
+            ;;
+        x86_64-apple-ios)
+            rustup target install x86_64-apple-ios
             ;;
     esac
-}
 
-install_rustup() {
-    # uninstall the rust toolchain installed by travis, we are going to use rustup
-    sh ~/rust/lib/rustlib/uninstall.sh
-
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-
-    rustc -V
-    cargo -V
-}
-
-install_standard_crates() {
-    if [ $(host) != "$TARGET" ]; then
-        rustup target add $TARGET
-    fi
-}
-
-configure_cargo() {
-    local prefix=$(gcc_prefix)
-
-    if [ ! -z $prefix ]; then
-        # information about the cross compiler
-        ${prefix}gcc -v
-
-        # tell cargo which linker to use for cross compilation
-        mkdir -p .cargo
-        cat >>.cargo/config <<EOF
-[target.$TARGET]
-linker = "${prefix}gcc"
-EOF
-    fi
-}
-
-main() {
-    install_c_toolchain
-    install_rustup
-    install_standard_crates
-    configure_cargo
-
-    # TODO if you need to install extra stuff add it here
-    # sudo dpkg --add-architecture i386
-    echo ARCHITECTURES:
-    dpkg --print-foreign-architectures
-    sudo apt-get install -y build-essential
-    # sudo apt-get install -y libc6:i386
-    # sudo apt-get install -y ia32-libs || sudo apt-get -fy install
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag $tag \
+           --target $target
 }
 
 main
